119a42f459b84491144aefd6f75b082f97023b0e
jaehyun.shim <jaehyun.shim@sebasi.co.kr>
2022-12-13 08:09:45 +0900

    rotate functions

--- src/main/kotlin/tree/AVLSearchTree.kt
+++ src/main/kotlin/tree/AVLSearchTree.kt
@@ -85,6 +85,27 @@ open class AVLSearchTree<T : Comparable<T>>() {
     return pivot
   }
 
+  private fun rightRotate(node: AVLNode<T>): AVLNode<T> {
+    val pivot = node.leftChild!!
+    node.leftChild = pivot.rightChild
+    pivot.rightChild = node
+    node.height = max(node.leftHeight, node.rightHeight) + 1
+    pivot.height = max(pivot.leftHeight, pivot.rightHeight) + 1
+    return pivot
+  }
+
+  private fun rightLeftRotate(node: AVLNode<T>): AVLNode<T> {
+    val rightChild = node.rightChild ?: return node
+    node.rightChild = rightRotate(rightChild)
+    return leftRotate(node)
+  }
+
+  private fun leftRightRotate(node: AVLNode<T>): AVLNode<T> {
+    val leftChild = node.leftChild ?: return node
+    node.leftChild = leftRotate(leftChild)
+    return rightRotate(node)
+  }
+
   override fun toString(): String {
     return root?.toString() ?: "empty tree"
   }
